aa6f108bbc4ef3bf4c5457796b8f588413fd3f56
Kuba <ja.tanski@gmail.com>
2019-09-25 21:07:20 +0200

    add boostrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './scss/global.scss';
-import App from './App';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "./scss/global.scss";
+import App from "./App";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
