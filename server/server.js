26f9f3ac673dc2326dd647c9b782d0eb2529bd59
unknown <aleksandra.przytula1995@gmail.com>
2019-09-27 06:55:18 +0200

    login v1

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-
+const users = require('./routes/users');
 require('./startup/db')();
 require('./startup/prod')(app);
 
+//-------------------------------------
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.use('/api/users', users);
