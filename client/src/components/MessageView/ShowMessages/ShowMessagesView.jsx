62e3c6e17604dbe6ef33f0fef1121e418df2159d
marcin.cyboran <marcin.cyboran.wsb@gmail.com>
2019-10-02 19:09:46 +0200

    update

--- client/src/components/MessageView/ShowMessages/ShowMessagesView.jsx
+++ client/src/components/MessageView/ShowMessages/ShowMessagesView.jsx
@@ -6,23 +6,23 @@ import {
     MDBListGroup,
     MDBListGroupItem,
     MDBCardFooter,
-    MDBBtn
+    MDBBtn,
 } from "mdbreact";
 
-const ShowMessagesView = ({messages, onLoadMore, disableBtn}) => {
+const ShowMessagesView = ({messages, load }) => {
 
-    const messagesJSXArr = messages.map(message => {
-        const timeDiff = Date.now() - message.when;
-        const timeDays = Math.ceil(timeDiff / 1000 / 60 / 60 / 24);
+    const messagesJSXArr = messages.reverse().map(message => {
+        const timeDiff = Date.now() - message.date;
+        const timeDays = Math.floor(timeDiff / 1000 / 60 / 60 / 24);
         return (
             <MDBListGroupItem 
-                key={message.from} 
+                key={message._id} 
                 hover >
                     <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1"> { message.from} </h5>
-                        <small>{ timeDays } day ago</small>
+                        <h5 className="mb-1"> { message.title } - <small>{ message.receivers[0].email } </small></h5>
+                        <small>{ timeDays === 0 ? 'today' : timeDays + ' day ago'}</small>
                     </div>
-                    <p className="mb-1">{ message.body }</p>
+                    <p className="mb-1">{ message.textContent }</p>
             </MDBListGroupItem>)
     });
 
@@ -36,14 +36,34 @@ const ShowMessagesView = ({messages, onLoadMore, disableBtn}) => {
                 </MDBListGroup>
 
             </MDBCardBody>
-            <MDBCardFooter>
+            <MDBCardFooter className="flex-footer">
+                <MDBBtn
+                    outline
+                    className="mr-auto"
+                    color="primary"
+                    size="sm"
+                    onClick={() => load('refresh')}
+                     > Refresh
+                </MDBBtn>
                 <MDBBtn 
                     color="deep-orange"
                     size="sm"
-                    onClick={onLoadMore}
-                    disabled={disableBtn}
-                     > Show All
+                    onClick={() => load('sent')}
+                     > Sent
                 </MDBBtn>
+                <MDBBtn 
+                    color="indigo"
+                    size="sm"
+                    onClick={() => load('received')}
+                     > Received
+                </MDBBtn>
+                <MDBBtn 
+                    color="unique"
+                    size="sm"
+                    onClick={() => load('bin')}
+                     > Bin
+                </MDBBtn>
+                
             </MDBCardFooter>
         </MDBCard>
     )
