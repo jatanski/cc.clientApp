e9c2d5cdfb376924750c53e9e6992ddd6217c8cb
micavanco <micavanco@gmail.com>
2019-10-03 19:10:47 +0200

    Fixed API payments endpoints urls

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -18,11 +18,11 @@ router.get('/:id', auth, async (req, res) => {
 });
 
 
-router.post('/:id', auth, async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findById(req.params.id);
+    let user = await User.findById(req.user._id);
     if (!user) return res.status(404).send('The user with the given ID was not found.');
 
     const date = Date.now();
