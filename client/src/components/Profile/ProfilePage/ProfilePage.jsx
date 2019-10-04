0f203740fea98fda1f8a9a37df1b5bbf071025e6
Sinn33 <bartosz.robak@hotmail.com>
2019-10-04 17:21:15 +0200

    data not hardcoded anymore

--- client/src/components/Profile/ProfilePage/ProfilePage.jsx
+++ client/src/components/Profile/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
 import ProfilePageView from './ProfilePageView';
+import baseModel from "../../../baseModel";
 class ProfilePage extends Component {
+
+    state = {user: ''}
+
+    componentDidMount = () => {
+        this.init();
+    }
+
+    init = async () => {
+
+        const token = baseModel.getAuthTokenHeaderObj()
+        const response = await fetch(baseModel.baseApiUrl + "users/", {
+          method: "GET",
+          headers: { 
+            "Content-Type": "application/json",
+            "x-auth-token": token["x-auth-token"]
+          },
+        });
+        const user = await response.json();
+        this.setState({
+            user: user
+        });        
+    }
 
     render() {
         return (
-            <ProfilePageView></ProfilePageView>
-        );
+            <ProfilePageView 
+                user={this.state.user}
+                >   
+            </ProfilePageView>
+        ) 
     }
 }
 
